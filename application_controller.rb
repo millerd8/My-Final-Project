7fcdffbb709273309ef4144bf80171874d763782
twant Want <taylor.want@gmail.com>
2016-07-22 15:33:13 +0000

    heroku deploy

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,17 @@
 require 'bundler'
 Bundler.require
-
+require_relative "models/pig_latinize.rb"
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  post '/result' do
+    @name=params[:name]
+    @pigname=to_pig_latin(@name)[0]
+    @image=to_pig_latin(@name)[1]
+    erb :results
   end
 
 end
--- models/pig_latinize.rb
+++ models/pig_latinize.rb
@@ -18,5 +18,5 @@ end
 
 
 def to_pig_latin(string)
-  string.split.collect{|word| piglatinize(word)}.join(" ")
+  [string.split.collect{|word| piglatinize(word)}.join(" "),"/images/IMG_0330.JPG"]
 end
